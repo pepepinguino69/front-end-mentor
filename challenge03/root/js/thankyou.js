c5c2d55a2e92403b5bca17c87a52036ec5884942
pepepinguino69 <ar@puntogovtech.com>
2022-07-17 16:11:49 -0300

    challenge 3 and minor mods to challenge 2)

---
+++ challenge03/root/js/thankyou.js
@@ -0,0 +1,19 @@
+ function validate() {
+    
+     var ele = document.getElementsByName('rating');
+     let r=0
+     for(i = 0; i < ele.length; i++) {
+         if(ele[i].checked){
+            document.getElementById('card').className = 'hidden';
+            document.getElementById('card2').className = 'thankyou';
+            r=ele[i].value;
+            document.getElementById("result").innerHTML
+            = "You selected " +r+" out of 5" }
+     }
+    }
+
+
+
+
+
+
